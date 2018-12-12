c54f19fe9037bf776090d1bfd71626bf4ac528df
Juan Rondán <juan.m.rondan@pwc.com>
2018-12-12 17:14:34 -0300

    add app.yaml for gcp deploy

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import { app } from './app';
 import './models/dbconnection';
 
+const PORT = process.env.PORT || 8080;
 app.listen(3000, function () {
-    console.log('listening on 3000');
+    console.log('listening on ${PORT}');
+    console.log('Press Ctrl-C to quit.');
 });
