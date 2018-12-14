a0c47b4bcbc2b217092e902bce1e167c20be4442
Juan Rondán <juan.m.rondan@pwc.com>
2018-12-14 16:40:09 -0300

    update db connection  tu use gcp

--- src/models/dbconnection.ts
+++ src/models/dbconnection.ts
@@ -2,6 +2,10 @@ import { connect, connection } from 'mongoose';
 //const mongoose = require( 'mongoose' );
 
 var dbURI = "mongodb://localhost/trainingApp";
+if (process.env.NODE_ENV === 'production') {
+    console.log("production environment detected");
+    dbURI = "mongodb://server/uma";
+}
 
 connect(dbURI);
 
